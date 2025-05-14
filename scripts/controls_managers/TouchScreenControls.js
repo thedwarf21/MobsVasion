384d10b33befce920ef8e1802f0ce867ce9bba5f
François ROQUEFORT <francois.roquefort@gmail.com>
2025-05-14 09:25:02 +0200

    feat: design et disposition du HUD de contrôle

--- scripts/controls_managers/TouchScreenControls.js
+++ scripts/controls_managers/TouchScreenControls.js
@@ -6,7 +6,10 @@ class TouchScreenControls {
 		// => idéalement, il faudrait délèguer la responsabilité de générer des informations exploitables (angle + force, au lieu de coordonnées de tapstart + coordonnées actuelles)
 		// et dans un format homogène à celui retourné par GamepadGenericAdapter pour les joysticks, de manière à mutualiser le traitement
 
-		document.querySelector('.hud .pause').addEventListener('click', function(e) { controller.togglePause(); });
+		document.querySelector('.hud .pause').addEventListener('mousedown', function(e) { 
+			e.stopPropagation(); 	// évite de déclencher un tir non souhaité, lorsque l'utilisateur appuie sur pause => deviendra obsolète quand le ViewportCompatibility permettra de shunter les contrôles souris
+			controller.togglePause();
+		});
 	}
 
 	/** Contrôles clavier et écran tactile => seront déplacés dans des classes dédiées */
