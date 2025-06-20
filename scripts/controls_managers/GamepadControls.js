59442de6e92d83315cb9fc459fa51854fdaaeb12
François ROQUEFORT <francois.roquefort@gmail.com>
2025-06-20 12:18:11 +0200

    feat: intégration de la nouvelle classe, pour la popup de config manette

--- scripts/controls_managers/GamepadControls.js
+++ scripts/controls_managers/GamepadControls.js
@@ -4,44 +4,25 @@ class GamepadControls {
 		window.addEventListener('gamepadconnected', (event)=> {
 			console.info("Manette connectée");
 
-			const controls = controller.scope.controls;
 			const gamepad = new GamepadGenericAdapter();
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.pause, "gamepad_config_pause", ()=> { controller.togglePause(); });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.primary_auto_fire, "gamepad_config_auto_aim", 
-										()=> { GamepadControls.#autoAim(); }, 
-										()=> { controls.firing_primary = false; }, true);
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.secondary_fire, "gamepad_config_rush", 
-										()=> { controls.firing_secondary = true; }, 
-										()=> { controls.firing_secondary = false; }, true);
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.reload, "gamepad_config_reload", 
-										()=> { controls.reloading = true; },
-										()=> { controls.reloading = false; });
-
-			const menu_controls = controls.gamepad_menu_nav;
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_up, 		"gamepad_config_up", 		()=> { menu_controls.up = true; });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_down, 	"gamepad_config_down", 		()=> { menu_controls.down = true; });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_left, 	"gamepad_config_left", 		()=> { menu_controls.left = true; });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_right, 	"gamepad_config_right", 	()=> { menu_controls.right = true; });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_validate, "gamepad_config_validate", 	()=> { menu_controls.validate = true; });
-			gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_cancel, 	"gamepad_config_abort", 	()=> { menu_controls.cancel = true; });
 			controller.timer.gamepad_mapper = gamepad;
-			
-			const was_paused = controls.paused;
-			controls.paused = true;
-			controller.scope.gamepadControlsUI = new GamepadConfigUI(gamepad, controller.language_manager, ()=> { controls.paused = was_paused; });
 
-			const active_popup = controller.popups_stack.activePopup();
-			if (active_popup)
-				active_popup.highlightActiveItem();
+			GamepadControls.#registerControlEntries(controller, gamepad)
+			GamepadControls.#showActiveItemInStillOpenedPopup();
+			GamepadControls.showConfigUI();
 		});
 
 		window.addEventListener('gamepaddisconnected', (event)=> {
-			controller.scope.gamepadControlsUI = null;
+			if (controller.gamepad_config_popup)
+				controller.popups_stack.pop();
+
 			controller.timer.gamepad_mapper = null;
 			GamepadControls.clearState(controller.scope.controls);
 			controller.togglePause();
 		});
 	}
+
+	static showConfigUI() { MainController.popups_stack.push(GamepadConfigPopup, MainController.timer.gamepad_mapper); }
 
 	static clearState(controls_state) {
 		for (const key in controls_state.gamepad_menu_nav)
@@ -79,6 +60,35 @@ class GamepadControls {
 			MainController.timer.gamepad_mapper.applyControl(GAMEPAD_ACTION_CODES.reload);
 			GamepadControls.#applyJoysticksControls();
 		}
+	}
+
+	static #registerControlEntries(controller, gamepad) {
+		const controls = controller.scope.controls;
+		const menu_controls = controls.gamepad_menu_nav;
+
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_up, 		"gamepad_config_up", 		()=> { menu_controls.up = true; });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_down, 	"gamepad_config_down", 		()=> { menu_controls.down = true; });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_validate, "gamepad_config_validate", 	()=> { menu_controls.validate = true; });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_left, 	"gamepad_config_left", 		()=> { menu_controls.left = true; });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_right, 	"gamepad_config_right", 	()=> { menu_controls.right = true; });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.menu_cancel, 	"gamepad_config_abort", 	()=> { menu_controls.cancel = true; });
+
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.pause, "gamepad_config_pause", ()=> { controller.togglePause(); });
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.primary_auto_fire, "gamepad_config_auto_aim", 
+									()=> { GamepadControls.#autoAim(); }, 
+									()=> { controls.firing_primary = false; }, true);
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.secondary_fire, "gamepad_config_rush", 
+									()=> { controls.firing_secondary = true; }, 
+									()=> { controls.firing_secondary = false; }, true);
+		gamepad.addControlEntry(GAMEPAD_ACTION_CODES.reload, "gamepad_config_reload", 
+									()=> { controls.reloading = true; },
+									()=> { controls.reloading = false; });
+	}
+
+	static #showActiveItemInStillOpenedPopup() {
+		const active_popup = MainController.popups_stack.activePopup();
+		if (active_popup)
+			active_popup.highlightActiveItem();
 	}
 
 	static #autoAim() {
